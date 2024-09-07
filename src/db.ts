1bcf5249281f44a0b16299ca5b4ed78be7c6b43a
Jeric June Logan <loganjeric25@gmail.com>
2024-09-07 10:55:30 +0800

    proper code colocation

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,5 @@
 import { Database } from 'bun:sqlite'
-import { BunSqliteStore } from 'hono-sessions/bun-sqlite-store'
 
 const db = new Database('./database.sqlite')
-const store = new BunSqliteStore(db)
 
-export { db, store }
+export { db }
--- src/session.ts
+++ src/session.ts
@@ -1,8 +1,11 @@
 import { Session as HonoSession, sessionMiddleware } from "hono-sessions"
-import { store } from "./db"
+import { BunSqliteStore } from 'hono-sessions/bun-sqlite-store'
+import { db } from "./db"
+
+const sessionStore = new BunSqliteStore(db)
 
 const session = sessionMiddleware({
-  store,
+  store: sessionStore,
   encryptionKey: Bun.env.AUTH_KEY,
   expireAfterSeconds: 900,
   cookieOptions: {
